aaa28d0fa15b7ce147728e44e116df8f3ba8b5c3
Keith Benjamin <kjb528i@yahoo.com>
2022-05-19 20:02:30 -0400

    route edits get all and delete all

--- src/main/java/com/careerdevs/stockapiv1/controllers/OverViewController.java
+++ src/main/java/com/careerdevs/stockapiv1/controllers/OverViewController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
 import java.util.zip.DataFormatException;
 
 @RestController
@@ -27,6 +28,20 @@ public class OverViewController {
 
 
     private final String BASE_URL = "https://www.alphavantage.co/query?function=OVERVIEW";
+
+    //GET ALL overview from sql database
+    // return [] of all overview
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll() {
+        try {
+            Iterable<Overview> allSymbols = overviewRepository.findAll();
+            return new ResponseEntity<>(allSymbols, HttpStatus.OK);
+        } catch (Exception e) {
+            return ApiErrorHandling.genericApiError(e);
+        }
+    }
+
 
     // http://localhost:4000/api/overview/test
     @GetMapping("/test")
@@ -66,6 +81,27 @@ public class OverViewController {
             }
             return ResponseEntity.ok(alphaVantageResponse.toString());
 
+        } catch (Exception e) {
+            return ApiErrorHandling.genericApiError(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOverviewById(@PathVariable("id") String id) {
+        try {
+            // control over error message and you get the 400. And code block is not needed.
+
+            long uID = Integer.parseInt(id);
+
+            Optional<Overview> foundUser = overviewRepository.findById(uID);
+
+            if (foundUser.isEmpty()) {
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, " User not found with ID: " + id);
+            }
+            return new ResponseEntity<>(foundUser, HttpStatus.OK);
+
+        } catch (HttpClientErrorException e) {
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
         } catch (Exception e) {
             return ApiErrorHandling.genericApiError(e);
         }
@@ -135,18 +171,7 @@ public class OverViewController {
             return ApiErrorHandling.genericApiError(e);
         }
     }
-    //GET ALL overview from sql database
-    // return [] of all overview
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAll() {
-        try {
-            Iterable<Overview> allSymbols = overviewRepository.findAll();
-            return new ResponseEntity<>(allSymbols, HttpStatus.OK);
-        } catch (Exception e) {
-            return ApiErrorHandling.genericApiError(e);
-        }
-    }
 
     //Delete ALL overview from sql database
     // return # of deletes overview
@@ -163,6 +188,32 @@ public class OverViewController {
         } catch (HttpClientErrorException e) {
             return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
 
+        } catch (Exception e) {
+            return ApiErrorHandling.genericApiError(e);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteOverviewById(@PathVariable("id") String id, RestTemplate restTemplate
+    ) {
+        try {
+
+            long uID = Integer.parseInt(id);
+
+            //check the range => other things to do
+
+            Optional<Overview> foundUser = overviewRepository.findById(uID);
+
+            if (foundUser.isEmpty()) {
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "User Not Found with ID: " + id);
+            }
+
+            overviewRepository.deleteById(uID);
+
+            return new ResponseEntity<>(foundUser, HttpStatus.OK);
+
+        } catch (HttpClientErrorException e) {
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
         } catch (Exception e) {
             return ApiErrorHandling.genericApiError(e);
         }
